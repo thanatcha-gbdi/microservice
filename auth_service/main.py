d7581a526d338757cecf686b41ef649b01631bff
thanatcha <thanatcha.kh@depa.or.th>
2023-07-27 10:08:36 +0700

    update way to extract the api key from the headers

--- auth_service/main.py
+++ auth_service/main.py
@@ -1,14 +1,13 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Header
 
 app = FastAPI()
 
 @app.post("/verify")
-async def verify(request: Request):
-    apiKey = request.headers.get("X-Api-Key")
-    if not apiKey:
+async def verify(x_api_key: str = Header()):
+    if not x_api_key:
         raise HTTPException(status_code=400, detail="X-Api-Key header missing")
 
-    if apiKey == "thisiscool":
+    if x_api_key == "thisiscool":
         return {"authorized": True}
 
     raise HTTPException(status_code=401, detail="Unauthorized")
--- movie_service/main.py
+++ movie_service/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Header
 import requests
 
 app = FastAPI(root_path="/movies")
@@ -9,10 +9,9 @@ mocked_data = [
 ]
 
 @app.get("/")
-async def read_movies(request: Request):
-    q = request.headers.get("X-Api-Key")
-    if q:
-        response = requests.post("http://auth_service:80/verify", headers={"X-Api-Key": q})
+async def read_movies(x_api_key: str = Header()):
+    if x_api_key:
+        response = requests.post("http://auth_service:80/verify", headers={"X-Api-Key": x_api_key})
         if response.status_code == 200:
             return {"data": mocked_data}
         else:
